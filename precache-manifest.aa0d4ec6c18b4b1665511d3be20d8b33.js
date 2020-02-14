848b81ceb09abb6eb21c9e335a0db712f95be045
Edgar_Q <edgarq_20@outlook.com>
2020-02-13 21:39:23 -0500

    Updates

---
+++ precache-manifest.aa0d4ec6c18b4b1665511d3be20d8b33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e20e1e0ca10dbca74b27e14c2147f3",
+    "url": "/cibertect-react/index.html"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/cibertect-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "89073398bfc762a9ec12",
+    "url": "/cibertect-react/static/css/main.12e04618.chunk.css"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/cibertect-react/static/js/2.074ebaf5.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/cibertect-react/static/js/2.074ebaf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89073398bfc762a9ec12",
+    "url": "/cibertect-react/static/js/main.06b1412a.chunk.js"
+  },
+  {
+    "revision": "b751fdba72abb6c4ef49",
+    "url": "/cibertect-react/static/js/runtime-main.2ba4f5ef.js"
+  },
+  {
+    "revision": "a1e184b8b64d0a5850f0b9a8dad670f8",
+    "url": "/cibertect-react/static/media/loading.a1e184b8.svg"
+  }
+]);
